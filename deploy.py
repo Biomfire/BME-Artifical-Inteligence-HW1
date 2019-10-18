4ba31e0037d0d3fef9d9deb1c1b01d0aeecc7196
Kiss Benedek <kissbence111@gmail.com>
2019-10-18 23:10:41 +0000

    Fixed deploy

--- deploy.py
+++ deploy.py
@@ -4,7 +4,7 @@ from zipfile import ZipFile
 from zipfile import ZIP_DEFLATED
 dir = os.path.dirname(__file__)
 files = glob.glob(os.path.join(dir, 'CarPlacer', '[!__]*.py'))
-#files.remove(os.path.join(dir, 'CarPlacer', 'main.py'))
+files.remove(os.path.join(dir, 'CarPlacer', 'main.py'))
 if not os.path.exists(os.path.join(dir, 'deploy')):
     os.makedirs((os.path.join(dir, 'deploy')))
 deployedMain = open("deploy/main.py", "w+")
@@ -21,5 +21,5 @@ deployedMain.write("import sys \nf = sys.stdin \n")
 deployedMain.write(f.read())
 deployedMain.close()
 with ZipFile('deploy/my_python_files.zip','w') as zip:
-    zip.write(mainPath, os.path.basename(mainPath), ZIP_DEFLATED)
-os.remove(mainPath)
+    zip.write(mainPath, os.path.realpath('deploy/main.py'), ZIP_DEFLATED)
+os.remove(os.path.realpath('deploy/main.py'))
