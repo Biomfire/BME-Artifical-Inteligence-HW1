60bcdafd68366ec4c351967b9af8150716b501d0
Kiss Benedek <kissbence111@gmail.com>
2019-10-18 23:06:34 +0000

    Added main with test algorithm

---
+++ CarPlacer/main.py
@@ -0,0 +1,33 @@
+import os
+
+from CarPlacer.map import Map
+from CarPlacer.car import Car
+f = open(os.path.realpath('../test_inputs/test2'))
+carlist = []
+input = f.readline().split("\t")
+m = Map(int(input[0]), int(input[1]))
+for _ in range(int(f.readline())):
+    input = f.readline().split("\t")
+    carlist.append(Car(int(input[0]), int(input[1]), _ + 1))
+carlist.sort(reverse=True)
+donecar = []
+
+for car in carlist:
+    done = False
+    for y in range(m.sizeY):
+        if done:
+            donecar.append(car)
+            break
+        for x in range(m.sizeX):
+            if m.placeCar(x, y, car):
+                # TODO remove variable from loop
+                done = True
+                break
+            else:
+                car.rotate()
+                if m.placeCar(x, y, car ):
+                    done = True
+                    break
+                else:
+                    car.rotate()
+m.print()
