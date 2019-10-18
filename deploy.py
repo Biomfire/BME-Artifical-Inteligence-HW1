2f99606c733d264de73e1b76b1da2fcf0d4c9769
Kiss Benedek <kissbence111@gmail.com>
2019-10-18 23:07:47 +0000

    Added deploy algorithm for easier uploading

---
+++ deploy.py
@@ -0,0 +1,25 @@
+import glob
+import os
+from zipfile import ZipFile
+from zipfile import ZIP_DEFLATED
+dir = os.path.dirname(__file__)
+files = glob.glob(os.path.join(dir, 'CarPlacer', '[!__]*.py'))
+#files.remove(os.path.join(dir, 'CarPlacer', 'main.py'))
+if not os.path.exists(os.path.join(dir, 'deploy')):
+    os.makedirs((os.path.join(dir, 'deploy')))
+deployedMain = open("deploy/main.py", "w+")
+for file in files:
+    f = open(file, "r")
+    deployedMain.write(f.read());
+    deployedMain.write("\n")
+mainPath = os.path.join(dir, 'CarPlacer', 'main.py')
+f = open(mainPath)
+for line in f:
+    if line == "#END\n":
+        break
+deployedMain.write("import sys \nf = sys.stdin \n")
+deployedMain.write(f.read())
+deployedMain.close()
+with ZipFile('deploy/my_python_files.zip','w') as zip:
+    zip.write(mainPath, os.path.basename(mainPath), ZIP_DEFLATED)
+os.remove(mainPath)
